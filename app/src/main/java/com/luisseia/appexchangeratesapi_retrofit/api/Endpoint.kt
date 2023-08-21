179b70ef477a72a668cfa064940957787b6be7cd
Luis-Seia <luis.seiax@gmail.com>
2023-08-21 20:07:14 +0200

    implemented convertion

--- app/src/main/java/com/luisseia/appexchangeratesapi_retrofit/api/Endpoint.kt
+++ app/src/main/java/com/luisseia/appexchangeratesapi_retrofit/api/Endpoint.kt
@@ -10,4 +10,7 @@ interface Endpoint {
     fun getCurrencies() : Call<JsonObject>
     @GET("/gh/fawazahmed0/currency-api@1/latest/currencies/{from}/{to}.json")
     fun getCurrenciesRate(@Path(value ="from", encoded = true) from: String, @Path(value ="to", encoded = true) to: String) : Call<JsonObject>
+
+    @GET("/v1/latest?access_key={api_key}&base={coin}")
+    fun getExchangeRates(@Path(value ="api_key", encoded = true) api_key: String, @Path(value ="coin", encoded = true) coin: String) : Call<JsonObject>
 }
